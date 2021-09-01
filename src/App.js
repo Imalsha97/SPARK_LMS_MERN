4e73063b2427f5ad40a5f309139be50ae2d70c0e
Imalsha <imalsham092@gmail.com>
2021-09-01 08:13:12 -0700

    add main layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import styled from "styled-components/";
+import {Header,Main, Footer} from "./components/Layout";
 
-const Title = styled.h1`
-  font-size: 3em;
-  text-align : center;
-  color: palevioletred;
-`;
-
-const Wrapper = styled.section`
-    padding : 4rem;
-    background-color : papayawhip;
-`;
 
 function App() {
   return (
-    <Wrapper>
-      <Title>
-      <h1>Imalsha</h1>
-    </Title>
-    </Wrapper>
+    //can only return one tag
+    <>
+    <Header>this is header</Header>
+    <Main>this body</Main>
+    <Footer>this footer</Footer>
+    </>
     
     
   );
